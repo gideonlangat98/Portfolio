dba5d59c2497e073b49f26e6ebcac782b0f35a83
gideonlangat98 <gideonlangat98@gmail.com>
2022-07-04 11:27:57 +0300

    declare variables and add fixed class to navbar

---
+++ Assets/js/script.js
@@ -0,0 +1,11 @@
+const navbar = document.getElementById('#nav');
+const closeBtn = document.getElementById('#close-btn');
+const sidebar = document.getElementById('#sidebar');
+const date = document.getElementById('#date');
+
+//add fixed class to navbar
+window.addEventListener('scroll', function(){
+    if(window)
+})
+
+
